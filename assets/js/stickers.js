19b724e790e46b78022b1f7458f08f4d10f5acf1
CamilaJaviera <camila.alarconf@gmail.com>
2022-11-13 18:15:41 -0300

    modified JS of stickers files

--- assets/js/stickers.js
+++ assets/js/stickers.js
@@ -9,6 +9,10 @@ btn.addEventListener("click", ()=>{
     const sticker1=+fox.value;
     const sticker2=+pig.value;
     const sticker3=+dog.value;
+    if (sticker1<0 || sticker2<0 || sticker3<0){
+        alert("Ingrese un valor mayor a 0");
+        return;
+    }
     const sumaStickers=sticker1+sticker2+sticker3;
     console.log(sumaStickers);
 
